7f55874b2570ad819415e84c1895e1394eb63d37
zhamri <zhamri@gmail.com>
2025-11-01 08:04:51 +0800

    update

---
+++ Problem-1.js
@@ -0,0 +1,9 @@
+function calculateTotal(price, quantity) {
+    return price * quantity;
+}
+
+let total = calculateTotal(10, 2);
+// let total = calculateTotal("10", 2); // returns 20 instead of error!
+// let total = calculateTotal("ten", 2); // NaN
+
+console.log("Total:", total);
